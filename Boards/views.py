c160c7a6f386aa4d665b069cf79b4ddd8938f0a4
Saravanan G <sgoffl1603@gmail.com>
2017-12-27 13:24:21 +0530

    Added html and models

--- Boards/views.py
+++ Boards/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from Boards.models import *
 # Create your views here.
+def index(request):
+    return render(request,'Boards/home.html')
+
